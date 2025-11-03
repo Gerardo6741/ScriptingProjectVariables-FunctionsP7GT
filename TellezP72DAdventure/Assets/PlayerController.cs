370dfc23061336b949032bef7d54949bd6582984
Gerardo6741 <759369@student.dusd.net>
2025-11-03 14:08:32 -0800

    Update PlayerController.cs

--- TellezP72DAdventure/Assets/PlayerController.cs
+++ TellezP72DAdventure/Assets/PlayerController.cs
@@ -8,9 +8,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
-        transform.position = position;
+        
     }
 
     // Update is called once per frame
