067f49ffff348bdb40ee970fb7860df8ed1649ad
Gerardo6741 <759369@student.dusd.net>
2025-11-14 15:26:59 -0800

    added new code to move and stuff yk the deaal
    added new code to move and stuff yk the deaal

--- TellezP72DAdventure/Assets/UnityTechnologies/2DBeginnerAdventureGame/Scripts/PlayerController.cs
+++ TellezP72DAdventure/Assets/UnityTechnologies/2DBeginnerAdventureGame/Scripts/PlayerController.cs
@@ -1,27 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public InputAction LeftAction;
+    public InputAction MoveAction;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        //  LeftAction.Enable();
+        MoveAction.Enable();
     }
+
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 0.1f;
 
-        Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
-
-
-
-            transform.position = position;
+        transform.position = position;
     }
 }
