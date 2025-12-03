1336570f4554ae0e90c14acb6e42ad3df7d93698
Gerardo6741 <759369@student.dusd.net>
2025-12-03 15:18:23 -0800

    Added barriers and collisions and decorative objects
    Added barriers and collisions and decorative objects

--- TellezP72DAdventure/Assets/UnityTechnologies/2DBeginnerAdventureGame/Scripts/PlayerController.cs
+++ TellezP72DAdventure/Assets/UnityTechnologies/2DBeginnerAdventureGame/Scripts/PlayerController.cs
@@ -6,23 +6,29 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public InputAction MoveAction;
-    public float speed = 6.0f;
+    public float speed = 10.0f;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
-        //  LeftAction.Enable();
         MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
+        move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * 0.1f * Time.deltaTime;
+    }
 
-        transform.position = position;
+
+    void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidbody2d.position + move * 5.0f * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
     }
 }
